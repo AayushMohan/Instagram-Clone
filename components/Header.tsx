d03b2595cb41805f39a695f4420f3adfab2de655
AayushMohan <aayushmohan1702@gmail.com>
2022-06-26 20:57:35 +0530

    Added Insta Logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      {/* Left */}
+      <div>
+        <Image src="https://links.papareact.com/ocw" layout="fill" />
+      </div>
+
+      {/* Middle */}
+
+      {/* Right */}
+    </div>
+  );
 };
 
 export default Header;
