29215a5daa6e16dda68be31072c8763f07cff78e
AayushMohan <aayushmohan1702@gmail.com>
2022-06-26 21:20:38 +0530

    Added: IG logo for sm devices

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,9 +6,16 @@ const Header = () => {
     <div>
       <div className="flex justify-between max-w-6xl">
         {/* Left */}
-        <div className="relative w-24 h-24">
+        <div className="relative w-24 h-24 hidden lg:inline-grid cursor-pointer">
           <Image
             src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/jjm"
             layout="fill"
             objectFit="contain"
           />
