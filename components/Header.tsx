8e7cd846f4f32b56acbb83841b14d3625857740c
AayushMohan <aayushmohan1702@gmail.com>
2022-06-26 21:13:58 +0530

    Added Styles in logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,14 +4,20 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      {/* Left */}
-      <div>
-        <Image src="https://links.papareact.com/ocw" layout="fill" />
+      <div className="flex justify-between max-w-6xl">
+        {/* Left */}
+        <div className="relative w-24 h-24">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        {/* Middle */}
+
+        {/* Right */}
       </div>
-
-      {/* Middle */}
-
-      {/* Right */}
     </div>
   );
 };
