91b92a4a4580d66c45c2da6c1edb055d262b7dc7
AayushMohan <aayushmohan1702@gmail.com>
2022-06-26 23:00:04 +0530

    Added Messenger Icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -47,8 +47,12 @@ const Header = () => {
 
         {/* Right */}
         <div className="flex items-center justify-end space-x-4">
-          <HomeIcon className="h-10 w-10" />
+          <HomeIcon className="h-10 w-10 cursor-pointer" />
           <MenuIcon className="h-6 md:hidden cursor-pointer" />
+          <img
+            src="https://img.icons8.com/ios/50/000000/facebook-messenger--v1.png"
+            className="h-10 w-10 cursor-pointer"
+          />
         </div>
       </div>
     </div>
