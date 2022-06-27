08b92d350755676bb8263e71b00b89eb8ffcccb5
AayushMohan <aayushmohan1702@gmail.com>
2022-06-27 23:24:46 +0530

    Added: Styles in main section

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -3,7 +3,7 @@ import Stories from "./Stories";
 
 const Feed = () => {
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2">
+    <main className="grid grid-cols-1 md:grid-cols-2 max-w-3xl xl:grid-cols-3 xl:max-x-6xl">
       <section>
         {/* Stories */}
         <Stories />
