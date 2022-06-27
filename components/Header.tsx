1bb7140fe314c059b4cbaa9b6407627bdbd4d0b5
AayushMohan <aayushmohan1702@gmail.com>
2022-06-27 22:30:42 +0530

    Done: Header Section

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,37 +1,32 @@
 import Image from "next/image";
 import React from "react";
-import {
-  SearchIcon,
-  PlusIcon,
-  HeartIcon,
-  MenuIcon,
-} from "@heroicons/react/outline";
+import { SearchIcon, PlusIcon, HeartIcon } from "@heroicons/react/outline";
 
 import { HomeIcon } from "@heroicons/react/solid";
 
 const Header = () => {
   return (
     <div className="shadow-sm border-b bg-white sticky top-0 z-50 ">
-      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
+      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto m-2">
         {/* Left */}
-        <div className="relative w-24  hidden lg:inline-grid cursor-pointer">
+        <div className="relative w-24 lg:inline-grid cursor-pointer">
           <Image
             src="https://links.papareact.com/ocw"
             layout="fill"
             objectFit="contain"
           />
         </div>
-        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
-          <Image
-            src="https://links.papareact.com/jjm"
-            layout="fill"
-            objectFit="contain"
+        {/* Mobile Setup */}
+        <div className="flex items-start order-first w-1/4 md:hidden">
+          <img
+            src="https://img.icons8.com/fluency-systems-regular/48/000000/camera.png"
+            className="navBtn h-7 mt-.5 block"
           />
         </div>
 
         {/* Middle - Search input field*/}
         <div>
-          <div className="relative mt-1 p-3 rounded-md">
+          <div className="relative mt-1 p-3 rounded-md hidden md:block">
             <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
               <SearchIcon className="h-5 w-5 text-gray-500" />
             </div>
@@ -42,15 +37,13 @@ const Header = () => {
             />
           </div>
         </div>
-
         {/* Right */}
         <div className="flex items-center justify-end space-x-4">
           <HomeIcon className="navBtn" />
-          <MenuIcon className="h-6 md:hidden cursor-pointer" />
-          <div className="relative navBtn">
+          <div className="relative navBtn block">
             <img
               src="https://img.icons8.com/fluency-systems-regular/96/000000/facebook-messenger.png"
-              className="navBtn"
+              className=" block h-7 md:h-8"
             />
             <div className="absolute -top-1 -right-3 flex items-center justify-center animate-pulse text-white text-xs w-5 h-5 bg-red-500 rounded-full">
               3
@@ -70,7 +63,7 @@ const Header = () => {
           <img
             src="https://avatars.githubusercontent.com/u/66319691?v=4"
             alt="Profile Pic"
-            className="h-10 cursor-pointer rounded-full "
+            className="h-10 cursor-pointer rounded-full hidden md:block"
           />
         </div>
       </div>
