42c03f6c0d8d30f59ea9042a374931376bd5215b
AayushMohan <aayushmohan1702@gmail.com>
2022-06-28 18:28:07 +0530

    Added Avatar Animation

--- components/Story.tsx
+++ components/Story.tsx
@@ -4,7 +4,7 @@ const Story = ({ img, username }: any) => {
   return (
     <div>
       <img
-        className="h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain cursor-pointer"
+        className="h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain cursor-pointer hover:scale-110 transition transform duration-200 ease-out"
         src={img}
         alt=""
       />
