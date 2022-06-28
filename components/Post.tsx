a782147f645c98bf0f9087fde588bfb6df708f89
AayushMohan <aayushmohan1702@gmail.com>
2022-06-28 19:43:36 +0530

    Added: Username

--- components/Post.tsx
+++ components/Post.tsx
@@ -4,6 +4,10 @@ const Post = ({ id, username, userImg, img, caption }: any) => {
   return (
     <div>
       {/* Header */}
+      <div>
+        <img src="" alt="" />
+        <p>{username}</p>
+      </div>
 
       {/* Image */}
 
