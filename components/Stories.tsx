c313fafb0923fa769d9ff35f5e53e546d356d587
AayushMohan <aayushmohan1702@gmail.com>
2022-06-28 17:18:26 +0530

    Added Story Component

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import faker from "faker";
+import Story from "./Story";
 
 const Stories = () => {
   const [suggestions, setSuggestions] = useState([]);
@@ -14,9 +15,13 @@ const Stories = () => {
   }, []);
 
   return (
-    <div>
+    <div className="flex space-x-2 p-6 bg-white mt-8">
       {suggestions.map((profile) => (
-        <Story key={profile.id} />
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username}
+        />
       ))}
       {/* Story */}
       {/* Story */}
