10dcc49a30e0ef98cb3b45ac602614d123802b5d
AayushMohan <aayushmohan1702@gmail.com>
2022-06-28 23:14:24 +0530

    Added Comment Section

--- components/Post.tsx
+++ components/Post.tsx
@@ -48,8 +48,14 @@ const Post = ({ id, username, userImg, img, caption }: any) => {
       {/* Comments */}
 
       {/* Input Box*/}
-      <form action="">
-        <EmojiHappyIcon />
+      <form className="flex items-center p-4">
+        <EmojiHappyIcon className="h-7" />
+        <input
+          type="text"
+          placeholder="Add a comment..."
+          className="border-none flex-1 focus:ring-0 outline-none"
+        />
+        <button className="font-semibold text-blue-400">Post</button>
       </form>
     </div>
   );
