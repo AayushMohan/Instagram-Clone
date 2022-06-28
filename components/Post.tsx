944030d22265d14982fdb113ac8be38bf418a970
AayushMohan <aayushmohan1702@gmail.com>
2022-06-28 19:37:22 +0530

    Added Posts

---
+++ components/Post.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Post = ({ id, username, userImg, img, caption }: any) => {
+  return <div>I'm a post</div>;
+};
+
+export default Post;
--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Post from "./Post";
+
+const posts = [
+  {
+    id: "123",
+    username: "thisisaayushmohan",
+    userImg: 'https://avatars.githubusercontent.com/u/66319691?v=4"',
+    img: 'https://avatars.githubusercontent.com/u/66319691?v=4"',
+    caption: "This is a DOPE!",
+  },
+  {
+    id: "124",
+    username: "thisisaayushmohan",
+    userImg: 'https://avatars.githubusercontent.com/u/66319691?v=4"',
+    img: 'https://avatars.githubusercontent.com/u/66319691?v=4"',
+    caption: "This is a DOPE!",
+  },
+];
 
 const Posts = () => {
-  return <div>Posts</div>;
+  return (
+    <div>
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          username={post.username}
+          userImg={post.userImg}
+          img={post.img}
+          caption={post.caption}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Posts;
