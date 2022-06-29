0663ba63d2ca0e82bde5e1c73ff0f229504c1478
AayushMohan <aayushmohan1702@gmail.com>
2022-06-29 13:40:15 +0530

    Added: Images

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -5,13 +5,25 @@ import { getProviders, signIn as SignIntoProvider } from "next-auth/react";
 const signIn = ({ providers }) => {
   return (
     <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => SignIntoProvider(provider.id)}>
-            Sign in with {provider.name}
-          </button>
-        </div>
-      ))}
+      <div className="relative flex mt-10 justify-center">
+        <img
+          className="items-center"
+          src="https://www.instagram.com/static/images/homepage/phones/home-phones.png/1dc085cdb87d.png"
+          alt=""
+        />
+        <img
+          src="https://www.instagram.com/static/images/homepage/screenshots/screenshot1-2x.png/cfd999368de3.png"
+          alt=""
+          className="h-[33.8rem] absolute top-[1.7rem] right-[38.5rem]"
+        />
+        {Object.values(providers).map((provider) => (
+          <div key={provider.name}>
+            <button onClick={() => SignIntoProvider(provider.id)}>
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
