03487abbbba94f64b0d3869d7a06e96ba5791d5b
AayushMohan <aayushmohan1702@gmail.com>
2022-06-29 15:27:27 +0530

    Added: Session For Mini Profiles

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,3 +1,4 @@
+import { useSession } from "next-auth/react";
 import React from "react";
 import MiniProfile from "./MiniProfile";
 import Posts from "./Posts";
@@ -5,8 +6,13 @@ import Stories from "./Stories";
 import Suggestions from "./Suggestions";
 
 const Feed = () => {
+  const { data: session } = useSession();
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 max-w-3xl xl:grid-cols-3 xl:max-x-6xl mx-auto">
+    <main
+      className={`grid grid-cols-1 md:grid-cols-2 max-w-3xl xl:grid-cols-3 xl:max-x-6xl mx-auto ${
+        !session && "grid-cols-1 !max-w-3xl"
+      }`}
+    >
       <section className="col-span-2">
         {/* Stories */}
         <Stories />
@@ -14,12 +20,14 @@ const Feed = () => {
         <Posts />
       </section>
 
-      <section className="hidden xl:inline-grid md:col-span-1">
-        <div className="fixed top-20">
-          <MiniProfile />
-          <Suggestions />
-        </div>
-      </section>
+      {session && (
+        <section className="hidden xl:inline-grid md:col-span-1">
+          <div className="fixed top-20">
+            <MiniProfile />
+            <Suggestions />
+          </div>
+        </section>
+      )}
     </main>
   );
 };
