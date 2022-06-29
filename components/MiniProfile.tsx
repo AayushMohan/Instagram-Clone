f00090671ddc6c956e8446e7b443355502ca8584
AayushMohan <aayushmohan1702@gmail.com>
2022-06-29 09:19:59 +0530

    Added Mini Profile Img

--- components/MiniProfile.tsx
+++ components/MiniProfile.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const MiniProfile = () => {
-  return <div></div>;
+  return (
+    <div>
+      <img
+        className="w-16 h-16 rounded-full border p-[2px]"
+        src="https://avatars.githubusercontent.com/u/66319691?v=4"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default MiniProfile;
