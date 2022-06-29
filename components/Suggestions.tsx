8e0c159195ef14ac63ef66ea2d4aa7120fbf3a3e
AayushMohan <aayushmohan1702@gmail.com>
2022-06-29 10:15:06 +0530

    Added: Faker

--- components/Suggestions.tsx
+++ components/Suggestions.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+import { faker } from "@faker-js/faker";
+import React, { useEffect, useState } from "react";
 
 const Suggestions = () => {
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const suggestions = [...Array(5)].map((_, i) => ({
+      ...faker.helpers.arrayElements(),
+      id: i,
+    }));
+
+    setSuggestions(suggestions);
+  }, []);
+
   return <div></div>;
 };
 
