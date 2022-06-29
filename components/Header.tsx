8bc8d78c8a5311c1a11332d884791d470f5b29fc
AayushMohan <aayushmohan1702@gmail.com>
2022-06-29 12:57:33 +0530

    Added: Session

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,11 @@ import React from "react";
 import { SearchIcon, PlusIcon, HeartIcon } from "@heroicons/react/outline";
 
 import { HomeIcon } from "@heroicons/react/solid";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+  const { data: session, status } = useSession();
+
   return (
     <div className="shadow-sm border-b bg-white sticky top-0 z-50 ">
       <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto m-2">
@@ -40,31 +43,39 @@ const Header = () => {
         {/* Right */}
         <div className="flex items-center justify-end space-x-4">
           <HomeIcon className="navBtn" />
-          <div className="relative navBtn block">
-            <img
-              src="https://img.icons8.com/fluency-systems-regular/96/000000/facebook-messenger.png"
-              className=" block h-7 md:h-8"
-            />
-            <div className="absolute -top-1 -right-3 flex items-center justify-center animate-pulse text-white text-xs w-5 h-5 bg-red-500 rounded-full">
-              3
-            </div>
-          </div>
 
-          <PlusIcon className="navBtn border-[2.5px] h-7 border-black rounded-lg" />
-          <img
-            src="https://img.icons8.com/material-outlined/96/000000/compass.png"
-            className="navBtn "
-          />
-          <img
-            src="https://img.icons8.com/ios-glyphs/48/000000/like--v2.png"
-            className="navBtn"
-          />
+          {session ? (
+            <>
+              <div className="relative navBtn block">
+                <img
+                  src="https://img.icons8.com/fluency-systems-regular/96/000000/facebook-messenger.png"
+                  className=" block h-7 md:h-8"
+                />
+                <div className="absolute -top-1 -right-3 flex items-center justify-center animate-pulse text-white text-xs w-5 h-5 bg-red-500 rounded-full">
+                  3
+                </div>
+              </div>
 
-          <img
-            src="https://avatars.githubusercontent.com/u/66319691?v=4"
-            alt="Profile Pic"
-            className="h-10 cursor-pointer rounded-full hidden md:block"
-          />
+              <PlusIcon className="navBtn border-[2.5px] h-7 border-black rounded-lg" />
+              <img
+                src="https://img.icons8.com/material-outlined/96/000000/compass.png"
+                className="navBtn "
+              />
+              <img
+                src="https://img.icons8.com/ios-glyphs/48/000000/like--v2.png"
+                className="navBtn"
+              />
+
+              <img
+                onClick={signOut}
+                src={session?.user?.image}
+                alt="Profile Pic"
+                className="h-10 w-10 cursor-pointer rounded-full hidden md:block"
+              />
+            </>
+          ) : (
+            <button onClick={signIn}>Sign In</button>
+          )}
         </div>
       </div>
     </div>
