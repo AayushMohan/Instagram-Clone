ce15ade2daf7047454f591b2757b4ab535154941
AayushMohan <aayushmohan1702@gmail.com>
2022-07-09 18:29:47 +0530

    renamed:    atoms/modalAtoms.tsx -> atoms/modalAtoms.js 	renamed:    pages/_app.tsx -> pages/_app.js 	renamed:    pages/index.tsx -> pages/index.js 	modified:   tsconfig.json

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 import Feed from "../components/Feed";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Modal from "../components/Modal";
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
